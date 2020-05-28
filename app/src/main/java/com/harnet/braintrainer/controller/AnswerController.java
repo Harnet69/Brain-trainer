0a534bfc5a2cf50396bbed8264a435a9c4378175
Harnet69 <harnet69@gmail.com>
2020-05-28 18:45:57 +0200

    Implemented shuffling answers

--- app/src/main/java/com/harnet/braintrainer/controller/AnswerController.java
+++ app/src/main/java/com/harnet/braintrainer/controller/AnswerController.java
@@ -5,6 +5,11 @@ import android.widget.TextView;
 
 import androidx.gridlayout.widget.GridLayout;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public class AnswerController {
     GridLayout answerGridLayout;
 
@@ -14,29 +19,50 @@ public class AnswerController {
 
     public void generateAnswers(int rightResult){
         System.out.println(rightResult);// TODO generate tags with results for four cells one is correct
+        int[] answers = new int[answerGridLayout.getChildCount()];
+        answers = fillAnswersArray(answers, rightResult);
         for (int i = 0; i < answerGridLayout.getChildCount(); i++) {
             View subView = answerGridLayout.getChildAt(i);
             if (subView instanceof TextView) {
-//                ImageView imageView = (ImageView) subView;
-                ((TextView) subView).setText(String.valueOf(rightResult));
-                subView.setTag(String.valueOf(rightResult));
-//                gameState[i] = 0;
+                ((TextView) subView).setText(String.valueOf(answers[i]));
+                subView.setTag(String.valueOf(answers[i]));
             }
         }
     }
 
-     // initial adding a tag with cell number to ImageView
-//    public void addTagsToCells() {
-//        GridLayout yourLayout = findViewById(R.id.gridLayout);
-////        Log.i("Num col", String.valueOf(yourLayout.getColumnCount()));  // OOP test
-//        gameState = new int[yourLayout.getChildCount()];
-//        for (int i = 0; i < yourLayout.getChildCount(); i++) {
-//            View subView = yourLayout.getChildAt(i);
-//            if (subView instanceof ImageView) {
-//                ImageView imageView = (ImageView) subView;
-//                imageView.setTag(i);
-//                gameState[i] = 0;
-//            }
-//        }
-//    }
+    private int[] fillAnswersArray(int[] answers, int rightResult){
+        Random rand = new Random();
+        int maxBound = (int) ((Math.abs(rightResult)*1.2)+rand.nextInt(10));
+        for(int i = 0; i < answers.length; i++){
+            if(i == 0){
+                answers[i] = rightResult;
+            }else{
+                answers[i] = rand.nextInt(maxBound);
+                if(isNumInArray(answers, answers[i])){
+                    i--;
+                }
+            }
+        }
+        return shuffleAnswers(answers);
+    }
+
+    // shuffle answers array
+    private int[] shuffleAnswers(int[] answers){
+        Random rand = new Random();
+        int answersLength = answers.length;
+        for (int i = 0; i < answersLength; i++) {
+            int randomIndexToSwap = rand.nextInt(answersLength);
+            int temp = answers[randomIndexToSwap];
+            answers[randomIndexToSwap] = answers[i];
+            answers[i] = temp;
+        }
+        return answers;
+    }
+
+    private boolean isNumInArray(int[] answers, int i){
+        for(int num : answers){
+            return i == num;
+        }
+        return false;
+    }
 }
